e94ab74a09389fb6861e8ac5c6d198795a7fbe18
lujingyu <rainieljy@hotmail.com>
2013-09-06 10:39:18 +0800

    new product detail

---
+++ OASAP/Class/ViewControllers/Home/ProductDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductDetailViewController.h
+//  OASAP
+//
+//  Created by jingyu lu on 9/6/13.
+//  Copyright (c) 2013 jingyu lu. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface ProductDetailViewController : RootViewController
+
+@end
